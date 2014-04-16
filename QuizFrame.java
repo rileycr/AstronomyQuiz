52bbb06823c207ef1461debaa49428983307ee52
Cooper Riley <rileycr@miamioh.edu>
2014-04-16 00:11:49 -0400

    Initial Commit

---
+++ QuizFrame.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.swing.*;
+
+/*
+  Authors:  Gavin Golden, Mark Gudorf, Victoria McIe, Cooper Riley
+  Class: CSE 385
+  Assignment: Astronomy Quiz
+  Instructor: Dr. Inclezan
+*/
+public class QuizFrame extends JFrame {
+
+}
