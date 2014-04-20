ef31b3d66f3a360b1e64a74962eeab251727fc75
Cooper Riley <rileycr@miamioh.edu>
2014-04-20 13:21:42 -0400

    More basic GUI work

--- AstroQuiz.java
+++ AstroQuiz.java
@@ -1,19 +1,42 @@
 package astronomy;
+import java.awt.*;
+import javax.swing.*;
 
 /*
- *  Authors:  Gavin Golden, Mark Gudorf, Victoria McIe, Cooper Riley
- *  Class: CSE 385
- *  Assignment: Astronomy Quiz
- *  Instructor: Dr. Inclezan
+  Authors:  Gavin Golden, Mark Gudorf, Victoria McIe, Cooper Riley
+  Class: CSE 385
+  Assignment: Astronomy Quiz
+  Instructor: Dr. Inclezan
 */
 public class AstroQuiz {
-
+    
+    private static String player;
+    
     public static void main(String[] args){
         System.out.println("Hello");
-        startup();
+        
+        //GUI
+        editPlayer(JOptionPane.showInputDialog("Welcome!! Please enter your name: "));
+        guiStart();
+
+        //Questions
+
+        //Queries
+
+        
     }
 
-    public static void startup(){
+    /*
+      Changes the name of the current player
+    */
+    public static void editPlayer(String name){
+        player = name;
+    }
+
+    /*
+      Starts the GUI of the quiz
+    */
+    public static void guiStart(){
         QuizFrame test = new QuizFrame();
     }
 }
--- QuizFrame.java
+++ QuizFrame.java
@@ -11,12 +11,14 @@ import javax.swing.*;
 public class QuizFrame extends JFrame {
 
     public QuizFrame() {
+        
         super("Astronomy Quiz");
+        
         setLayout(new BorderLayout(1,1));
         setPreferredSize(new Dimension(500,500));
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
-
+                
         pack();
         setVisible(true);
     }
