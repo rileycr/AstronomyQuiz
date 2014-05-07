5d96c29da2776241d3f44cfcb4d2e1791f53f11b
Cooper Riley <rileycr@miamioh.edu>
2014-05-07 00:57:58 -0400

    Removed old stuff and formatting changes

--- AstroQuiz.java
+++ AstroQuiz.java
@@ -19,10 +19,7 @@ public class AstroQuiz {
 
     public Connection connection;
     private int qCount = 0;  // running count of the question
-<<<<<<< HEAD
 
-=======
->>>>>>> FETCH_HEAD
     private static Question[] quizQs = new Question[11];
     Random random = new Random();
     
@@ -77,99 +74,69 @@ public class AstroQuiz {
     /** Randomly selects a question type to generate -MFG*/
     
     private Question makeQuestion(){
-      String[] defaultInfo = {"Earth","Moon","Pegasus","Pluto"};
-      Question question;
-      try{
-      switch(random.nextInt(1) + 1) {
-        case 1: question = makeSunOrbitQuestion();
-        break;
-        default: question = new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
-      }
-      return question;
-      } catch (Exception e){
-        System.out.println(e.getMessage());
-        question =  new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
-      }
-      return question;
+        String[] defaultInfo = {"Earth","Moon","Pegasus","Pluto"};
+        Question question;
+        try{
+            switch(random.nextInt(1) + 1) {
+            case 1: question = makeSunOrbitQuestion();
+                break;
+            default: question = new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
+            }
+            return question;
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            question =  new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
+        }
+        return question;
     }
     
     /** Generates question by querying database */
     private Question makeSunOrbitQuestion() throws SQLException{
-      String[] defaultInfo = {"Earth","Moon","Pegasus","Pluto"};
-      ResultSet planets = execQuery("SELECT Name FROM Planet");
-      ArrayList<String> planetNames = new ArrayList<String>();
-      do{
-        planetNames.add(planets.getString("Name"));
-      } while (planets.next());
-      String correctPlanet = planetNames.get(random.nextInt(planetNames.size()));
-      String[] questionInfo = new String[4];
-      questionInfo[0] = correctPlanet;
-      System.out.println("Correct Planet: " + correctPlanet);
-      int count = 1;
-      while(count < 4){
-        String wrongPlanet = planetNames.get(random.nextInt(planetNames.size()));
-        System.out.println("Count: " + count + " \nWrong Planet: " + wrongPlanet);
-        if(!(wrongPlanet.equals(correctPlanet))){
-          questionInfo[count] = wrongPlanet;
-          count++;
+        String[] defaultInfo = {"Earth","Moon","Pegasus","Pluto"};
+        ResultSet planets = execQuery("SELECT Name FROM Planet");
+        ArrayList<String> planetNames = new ArrayList<String>();
+        do{
+            planetNames.add(planets.getString("Name"));
+        } while (planets.next());
+        String correctPlanet = planetNames.get(random.nextInt(planetNames.size()));
+        String[] questionInfo = new String[4];
+        questionInfo[0] = correctPlanet;
+        System.out.println("Correct Planet: " + correctPlanet);
+        int count = 1;
+        while(count < 4){
+            String wrongPlanet = planetNames.get(random.nextInt(planetNames.size()));
+            System.out.println("Count: " + count + " \nWrong Planet: " + wrongPlanet);
+            if(!(wrongPlanet.equals(correctPlanet))){
+                questionInfo[count] = wrongPlanet;
+                count++;
+            }
         }
-      }
-      String query = "SELECT Orbits FROM Planet WHERE Name = \"";
-      String finalQuery = query.concat(correctPlanet);
-      finalQuery = finalQuery.concat("\";");
-      System.out.println("Query: " + finalQuery);
-      ResultSet starName = execQuery(finalQuery);
-      Collections.shuffle(Arrays.asList(questionInfo));
-      String answerLetter = "";
-      if(questionInfo[0].equals(correctPlanet)){
-        answerLetter = "A";
-      } else if (questionInfo[1].equals(correctPlanet)){
-        answerLetter = "B";
-      } else if (questionInfo[2].equals(correctPlanet)){
-        answerLetter = "C";
-      } else {
-        answerLetter = "D";
-      }
+        String query = "SELECT Orbits FROM Planet WHERE Name = \"";
+        String finalQuery = query.concat(correctPlanet);
+        finalQuery = finalQuery.concat("\";");
+        System.out.println("Query: " + finalQuery);
+        ResultSet starName = execQuery(finalQuery);
+        Collections.shuffle(Arrays.asList(questionInfo));
+        String answerLetter = "";
+        if(questionInfo[0].equals(correctPlanet)){
+            answerLetter = "A";
+        } else if (questionInfo[1].equals(correctPlanet)){
+            answerLetter = "B";
+        } else if (questionInfo[2].equals(correctPlanet)){
+            answerLetter = "C";
+        } else {
+            answerLetter = "D";
+        }
         
-      return new MCQuestion("Which planet orbits the star \"" + starName.getString("Orbits") + "\"?", questionInfo, answerLetter);
-      //return new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
-               
-               
+        return new MCQuestion("Which planet orbits the star \"" + starName.getString("Orbits") + "\"?", questionInfo, answerLetter);
+        //return new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
     }
-      
+    
     /**
        Currently in testing phase, creates questions to send to the GUI
     */
     private void createQuestions() {
-
         
-        
-        /*
-            Use randSet(int setSize) to generate a random String[] of planetary objects.
-         
-            ---- How to randomize the order of the set with the actual answer included...?
-         */
-/*
-        String[] options1 = randSet(4, "Planet");
-        options1 = randInsert(options1, "Jupiter");
-        
-        String[] options2 = randSet(4, "");
-        options2 = randInsert(options2, "Eliptical, Cloud, Spiral");
-        
-        String[] options3 = randSet(4, "Star");
-        options3 = randInsert(options3, "Jupiter");
-        
-        String[] options4 = randSet(4,"");
-        options4[3]= "Saturn";
-        options4[2] = "Mars";
-        options4[1] = "Neptune";
-        
-        String[] options5 = randSet(4, "");
-        options5[0] = "Mars";
-        options5[2] = "Neptune";
-*/
-
-<<<<<<< HEAD
         int count = 0;
         while (count < 5) {
             Query q1 = new Query(0);
@@ -179,22 +146,7 @@ public class AstroQuiz {
             quizQs[count] = new MCQuestion(q1.question, q1.options, q1.options[4]);
             count ++;
         }
-        /*quizQs[1] = new MCQuestion("What are the three types of galaxies?", options2, options2[4]);
-=======
-
-        //quizQs[0] = new MCQuestion("Which planet orbits the sun?", options1, options1[4]);
-        quizQs[0] = makeQuestion();
-        quizQs[1] = new MCQuestion("What are the three types of galaxies?", options2, options2[4]);
->>>>>>> FETCH_HEAD
-        quizQs[2] = new ResponseQuestion("What is the mass of the earth?", "5.9726e24");
-        quizQs[3] = new MCQuestion("Which planet is more massive?", options3, options3[4]);
-        quizQs[4] = new MCQuestion("Which planet has the most moons orbiting it?", options4, "D");
-        quizQs[5] = new MCQuestion("Phobos and Deimos are moons of which planet?", options5, "A");
-        quizQs[6] = new MCQuestion("Triton is the largest moon of what planet?", options5, "C");
-        quizQs[7] = new ResponseQuestion("Sol is a star in which galaxy?", "The Milky Way");
-        quizQs[8] = new MCQuestion("Which planet has the least moons orbiting it?", options4, "B");
-        quizQs[9] = new MCQuestion("Which planet has two moons orbiting it?", options4, "C");
-*/
+        
         quizQs[10] = null;
 
     }
@@ -235,7 +187,10 @@ public class AstroQuiz {
         
         guiFrame.editQResultDisplay(table);
     }
-    
+
+    /**
+       Inserts correct answer into array
+    */
     private String[] randInsert(String answers[], String correct) {
         String letters[] = {"A", "B", "C", "D"};
         
@@ -249,74 +204,7 @@ public class AstroQuiz {
         return update;
         
     }
-    
-    /**
-       Randomizes objects to be in questions,
-    */
-    private String randomObj(String not) throws SQLException {
-        
-        ArrayList<String> tables = getTableNames();
-        
-     // Get random
-        String table = "";
-        do {
-            int rTable = (int)(Math.random() * tables.size());
-            table = tables.get(rTable);
-        } while (table.equals(not));
-     
 
-        // Find the number of rows in the upcoming result set.
-     String countQuery = "SELECT COUNT(*) FROM " + table;
-        int rows = -1;
-        
-        try {
-            ResultSet rs = execQuery(countQuery);
-            rows = Integer.parseInt(rs.getString("COUNT(*)"));
-            rs.close();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-
-        }
-        // Prepare and execute query
-        String query = ("SELECT * FROM " + table);
-     ResultSet rs = execQuery(query);
-     
-     String randName = "";
-     
-        // Get object name from random row in result set
-     int rand = (int)(Math.random() * rows);
-        
-     try {
-            for(int i = 0; (i < rand && rs.next()); i++) {
-                randName = rs.getString("Name");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            rs.close();
-        }
-     return randName;
-    }
-    
-    private String[] randSet(int num, String not) { /***** changed 'not'*****/
-        String answerSet[] = new String[num];
-        for(int i = 0; (i < num); i++) {
-            String s = "";
-            while(s == "") {
-                try{
-                    s = randomObj(not); /***** changed 'not' *****/
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            answerSet[i] = s;
-        }
-        return answerSet;
-    }
-    
-    
-    
     /**
         Get list of names for all tables in database
      */
@@ -343,7 +231,6 @@ public class AstroQuiz {
         }
         
         return tables;
-
     }
 
     /**
@@ -380,7 +267,6 @@ public class AstroQuiz {
      * PROGRAM LAUNCH.
      */
     public static void main(String[] args){
-      System.out.println("Running");
         try {
             // Set System L&F
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
