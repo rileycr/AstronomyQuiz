177839d1810cbde00e3e66c733d761b308b4eb5c
gudorfmf <gudorfmf@miamioh.edu>
2014-05-08 15:01:44 -0400

    Fixed syncing issues

--- AstroQuiz.java
+++ AstroQuiz.java
@@ -71,65 +71,75 @@ public class AstroQuiz {
             e.printStackTrace();
         }
     }
-    /** Randomly selects a question type to generate -MFG*/
+   /** Randomly selects a question type to generate */
     
     private Question makeQuestion(){
-        String[] defaultInfo = {"Earth","Moon","Pegasus","Pluto"};
-        Question question;
-        try{
-            switch(random.nextInt(1) + 1) {
-            case 1: question = makeSunOrbitQuestion();
-                break;
-            default: question = new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
-            }
-            return question;
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-            question =  new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
-        }
-        return question;
+
+      String[] defaultInfo = {"Earth","Moon","Pegasus","Pluto"};
+      Question question;
+      try{
+      switch(random.nextInt(2) + 1) {
+        case 1: question = makeSunOrbitQuestion();
+        break;
+        //case 2: question = makePlanetOrbitQuestion();
+        //break;
+        default: question = new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
+      }
+      return question;
+      } catch (Exception e){
+        System.out.println(e.getMessage());
+        System.out.println("Error creating question");
+        question =  new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
+      }
+      return question;
+
     }
     
-    /** Generates question by querying database */
+    /** Generates question by querying database 
+      * This question is about planets orbiting a sun*/
     private Question makeSunOrbitQuestion() throws SQLException{
-        String[] defaultInfo = {"Earth","Moon","Pegasus","Pluto"};
-        ResultSet planets = execQuery("SELECT Name FROM Planet");
-        ArrayList<String> planetNames = new ArrayList<String>();
-        do{
-            planetNames.add(planets.getString("Name"));
-        } while (planets.next());
-        String correctPlanet = planetNames.get(random.nextInt(planetNames.size()));
-        String[] questionInfo = new String[4];
-        questionInfo[0] = correctPlanet;
-        int count = 1;
-        while(count < 4){
-            String wrongPlanet = planetNames.get(random.nextInt(planetNames.size()));
-   
-            if(!(wrongPlanet.equals(correctPlanet))){
-                questionInfo[count] = wrongPlanet;
-                count++;
-            }
+      ResultSet planets = execQuery("SELECT Name, Orbits FROM Planet");
+      ArrayList<String[]> planetNames = new ArrayList<String[]>();
+      do{
+        String[] planetInfo = new String[2];
+        planetInfo[0] = planets.getString("Name");
+        planetInfo[1] = planets.getString("Orbits");
+        planetNames.add(planetInfo);
+      } while (planets.next());
+      String[] correctPlanetInfo = planetNames.get(random.nextInt(planetNames.size()));
+      String correctPlanet = correctPlanetInfo[0];
+      String[] questionInfo = new String[4];
+      questionInfo[0] = correctPlanet;
+      //System.out.println("Correct Planet: " + correctPlanet);
+      String correctPlanetOrbit = correctPlanetInfo[1];
+      //System.out.println("Correct Planet Orbits: " + correctPlanetOrbit);
+      int count = 1;
+      while(count < 4){
+        String wrongPlanet = planetNames.get(random.nextInt(planetNames.size()))[0];
+        String wrongPlanetOrbit = planetNames.get(random.nextInt(planetNames.size()))[1];
+        //System.out.println("Count: " + count + " \nWrong Planet: " + wrongPlanet);
+        if(!(wrongPlanet.equals(correctPlanet)) && !((wrongPlanet.equals(questionInfo[1])) || wrongPlanet.equals(questionInfo[2]) || wrongPlanet.equals(questionInfo[3])) && !(wrongPlanetOrbit.equals(correctPlanetOrbit))){
+          questionInfo[count] = wrongPlanet;
+          count++;
         }
-        String query = "SELECT Orbits FROM Planet WHERE Name = \"";
-        String finalQuery = query.concat(correctPlanet);
-        finalQuery = finalQuery.concat("\";");
-        ResultSet starName = execQuery(finalQuery);
-        Collections.shuffle(Arrays.asList(questionInfo));
-        String answerLetter = "";
-        if(questionInfo[0].equals(correctPlanet)){
-            answerLetter = "A";
-        } else if (questionInfo[1].equals(correctPlanet)){
-            answerLetter = "B";
-        } else if (questionInfo[2].equals(correctPlanet)){
-            answerLetter = "C";
-        } else {
-            answerLetter = "D";
-        }
-        
-        return new MCQuestion("Which planet orbits the star \"" + starName.getString("Orbits") + "\"?", questionInfo, answerLetter);
-        //return new MCQuestion("Which planet orbits the sun?", defaultInfo, "Earth");
+      }
+      Collections.shuffle(Arrays.asList(questionInfo));
+      return new MCQuestion("Which planet orbits the star " + correctPlanetOrbit, questionInfo, determineLetter(questionInfo, correctPlanet));
     }
     
+    /** Convenience method to find correct option letter */
+     private String determineLetter(String[] questionInfo, String correctAnswer){
+      if(questionInfo[0].equals(correctAnswer)){
+        return "A";
+      } else if (questionInfo[1].equals(correctAnswer)){
+        return "B";
+      } else if (questionInfo[2].equals(correctAnswer)){
+        return "C";
+      }
+      return  "D";
+    }
+      
+
     /**
        Currently in testing phase, creates questions to send to the GUI
     */
